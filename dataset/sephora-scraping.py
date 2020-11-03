5b6b5fa41634bf0f0ad4b8e893b223229ebe6d6f
xiwang129 <amy.xwang71@gmail.com>
2020-11-03 16:09:05 -0500

    fix img url issue

--- dataset/sephora-scraping.py
+++ dataset/sephora-scraping.py
@@ -20,23 +20,28 @@ driver = webdriver.Chrome(executable_path = chrome_path)
 
 url ='https://www.sephora.com'
 driver.get(url)
-time.sleep(10)
+time.sleep(5)
 
+df = pd.DataFrame(columns=['Label', 'URL'])
 
 tickers = ['face-wash-for-men','moisturizer-men','mens-grooming-kits-mens-shaving','mens-fragrance','eye-cream-men','deodorant-for-men-body-spray','mens-hair-products']
 
 for ticker in tickers:
     url = 'https://www.sephora.com/shop/' + ticker + '?pageSize=300'
     driver.get(url)
+   
 
-    xpath = '/html/body/div[5]/div/div/div[1]/div/div/button'
-    btn = driver.find_element_by_xpath(xpath)
-    btn.click()
-    time.sleep(20)
+    # xpath = '/html/body/div[5]/div/div/div[1]/div/div/button'
+    # btn = driver.find_element_by_xpath(xpath)
+    # btn.click()
+    # time.sleep(20)
 
-    browser = scrollDown(driver,10)
+    browser = scrollDown(driver,20)
     time.sleep(10)
-
+    browser = scrollDown(driver,20)
+    time.sleep(10)
+    browser = scrollDown(driver,20)
+    time.sleep(10)
     browser = scrollDown(driver, 10)
 
     element = driver.find_elements_by_class_name('css-ix8km1')
@@ -48,76 +53,69 @@ for ticker in tickers:
         subpageURL.append(subURL)
 
     dic = {'Label':ticker, 'URL': subpageURL}
-    df = pd.DataFrame(dic)
-
-df2 = pd.DataFrame(columns = ['brand_name','product_name','description','price','score','ingredients','skin_type','image_url'])
+    df =df.append(pd.DataFrame(dic), ignore_index = True)
+   
+df2 = pd.DataFrame(columns = ['brand_name','product_name','description','price','score','skin_type','ingredients','image_url'])
 df = pd.concat([df, df2],axis =1)
+
 
 for i in range(len(df)+1):
     url = df.URL[i]
     driver.get(url)
     time.sleep(5)
 
-    xpath = '/html/body/div[5]/div/div/div[1]/div/div/button'
-    btn = driver.find_elements_by_xpath(xpath)
-    btn.click()
-
-    #brand
-
-    df.brand_name[i] = driver.find_element_by_name('css-57kn72').text
+    #brand name
+    xpath ='/html/body/div[1]/div[2]/div/main/div/div[1]/div/div[2]/div[1]/div[1]/h1/a/span'
+    df.brand_name[i] = driver.find_element_by_xpath(xpath).text
 
     #price
-
-    xpath ='/html/body/div[1]/div[2]/div/main/div/div[1]/div/div[2]/div[1]/div[2]/div[1]/span'
-    df.price[i] = driver.find_element_by_xpath(xpath).text
+    xpath1 ='/html/body/div[1]/div[2]/div/main/div/div[1]/div/div[2]/div[1]/div[2]/div[1]/span'
+    df.price[i] = driver.find_element_by_xpath(xpath1).text
     
     # product name 
     xpath2 = '/html/body/div[1]/div[2]/div/main/div/div[1]/div/div[2]/div[1]/div[1]/h1/span'
     df.product_name[i] = driver.find_element_by_xpath(xpath2).text
 
     #image url 
-    img = driver.find_element_by_class_name('css-1rovmyu')
-    df.image_url[i] = img.get_attribute('src')
+    try:
+        img = driver.find_element_by_class_name('css-1rovmyu')
+        df.image_url[i] = img.get_attribute('src')
+    except NoSuchElementException:
+        df.image_url[i] = ' '
+  
+    # #skin type
+    # xpath = '//*[@id="tabpanel0"]/div/b[2]'
+    # skin_type = driver.find_element_by_xpath(xpath)
+
+    #product description
+    df.description[i] = driver.find_element_by_class_name('css-1rny024').text
+    
+    # ingredients
+    xpath3 = '//*[@id="tab2"]'
+    btn = driver.find_element_by_xpath(xpath3)
+    btn.click()
+
+    try:
+        df.ingredients[i] = driver.find_element_by_xpath('//*[@id="tabpanel2"]/div').text
+    except NoSuchElementException:
+        df.ingredients[i] = "No Info"
+
+
+    browser = scrollDown(driver,1)
+    time.sleep(5)
+    browser = scrollDown(driver,1)
+    time.sleep(5)
 
     # score 
 
     try: 
-        xpath3= '//*[@id="ratings-reviews"]/div[2]/div[2]/div[1]/div/div[1]/div[2]'
-        score = driver.find_element_by_xpath(xpath3).text
-        score = re.match('\d.\d', score).group()
+        a = driver.find_element_by_class_name('css-1k3n1un').text
+        score = re.match('\d.\d', a).group()
         df['score'][i] = str(score)
     
     except NoSuchElementException:
         df['score'][i] = 0
     
-    
-    #skin type
-
-    xpath = '//*[@id="tabpanel0"]/div/b[2]'
-    skin_type = driver.find_element_by_xpath(xpath)
-
-    # product desciption
-
-    xpath = '//*[@id="tabpanel0"]/div/b[1]'
-    descriptions = driver.find_elements_by_xpath(xpath)
-    
-    #ingredients
-
-    xpath = '//*[@id="tabpanel2"]'
-    btn = driver.find_element_by_xpath(xpath)
-    btn.click()
-
-    try:
-        df.ingredients[i] = driver.find_element_by_xpath('//*[@id="tabpanel2"]/div')
-    except NoSuchElementException:
-        df.ingredients[i] = "No Info"
-    
-
-    browser = scrollDown(driver,1)
-    time.sleep(5)
-    browser = scrollDown(driver,1)
-    time.sleep(5)
-
 
     df.to_csv('sephora_products.csv', encoding = 'utf-8')
 
---
+++ dataset/tempCodeRunnerFile.py
@@ -0,0 +1,4 @@
+ # xpath = '/html/body/div[5]/div/div/div[1]/div/div/button'
+    # btn = driver.find_element_by_xpath(xpath)
+    # btn.click()
+    # time.sleep(20)
