d625f71700277981e30a88fc8ef62a13efbc4e2c
xiwang129 <amy.xwang71@gmail.com>
2020-11-06 18:43:22 -0500

    first commit

---
+++ Quinn-ios/Quinn-ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Quinn-ios
+//
+//  Created by Xiang W. on 11/6/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
