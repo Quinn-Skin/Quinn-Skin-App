e57ef17ff8dc30a8f41048c736539fd87f643f48
Brayhan De Aza <brayhandeaza@gmail.com>
2020-11-21 14:41:18 -0500

    recommend storyboard with recommend product added

--- Quinn-ios/Quinn-ios/controllers/RecommendedProductViewController.swift
+++ Quinn-ios/Quinn-ios/controllers/RecommendedProductViewController.swift
@@ -9,16 +9,23 @@ import UIKit
 import AlamofireImage
 
 class RecommendedProductViewController: UIViewController {
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productName: UILabel!
+    @IBOutlet weak var productDescription: UILabel!
+    
     var produts = [String:Any]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.getOneProducts(id: "1")
+        
 
         // Do any additional setup after loading the view.
     }
     
-    func getOneProducts(id: String) -> Void {
+    override func viewDidAppear(_ animated: Bool) {
+        self.recommmendedProduct(id: "4")
+    }
+    func recommmendedProduct(id: String) -> Void {
         let url = URL(string: "https://quinn-server.herokuapp.com/products/\(id)")!
         let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
         let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
@@ -27,16 +34,30 @@ class RecommendedProductViewController: UIViewController {
             // This will run when the network request returns
             if let error = error {
                print(error.localizedDescription)
+                
             } else if let data = data {
-               let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
-
+                let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
                 self.produts = dataDictionary["Produt"] as! [String : Any]
-                print(self.produts)
+        
+                
+                // Product Image
+                let imageUrl = self.produts["image_url"] as? String
+                let formatedImageUrl = URL(string: imageUrl!)
+                self.productImage.af_setImage(withURL: formatedImageUrl!)
+
+                // Product name
+                let productNameText = self.produts["brand_name"] as? String
+                self.productName.text = productNameText?.capitalized
+                
+                // Product description
+                self.productDescription.text = self.produts["description"] as? String
             }
          }
          task.resume()
     }
 
+
+    
     /*
     // MARK: - Navigation
 
---
+++ Quinn-ios/Quinn-ios/controllers/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  Quinn-ios
+//
+//  Created by Brayhan De Aza on 11/21/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    var recommendeViewController: UIViewController!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func onPress(_ sender: Any) {
+        
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
