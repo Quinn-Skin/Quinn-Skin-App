9de6adc397126c56b152180450cfd1a15b1518cd
Amy <40009989+xiwang129@users.noreply.github.com>
2020-12-01 12:30:28 -0500

    Update Contents.swift

--- model-request.playground/Contents.swift
+++ model-request.playground/Contents.swift
@@ -2,40 +2,38 @@ import UIKit
 
 //let parameters = ["Label":2,"price":10,"score":5,"Combination":1,"Dry":0,"Normal":1,"Oily":0,"Sensitive":1]
 
-let parameters = [2,10,5,1,0,1,0,1]
-let url = URL(string: "https://quinn-model.herokuapp.com/predict")
+let parameters:[[Int]] = [[2,10,4,1,0,0,0,0]]
 
+let url = URL(string: "https://quinn-model.herokuapp.com/productid")
 let session = URLSession.shared
 
 var request = URLRequest(url: url!)
 request.httpMethod = "POST"
 
 do {
-    request.httpBody = try JSONSerialization.data(withJSONObject: parameters, options: JSONSerialization.WritingOptions.prettyPrinted)
+    request.httpBody = try JSONSerialization.data(withJSONObject: parameters,options: JSONSerialization.WritingOptions.prettyPrinted)
+    
 } catch let error{
     print(error.localizedDescription)
 }
 
-request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-request.addValue("application/json", forHTTPHeaderField: "Accept")
+request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+request.setValue("application/json", forHTTPHeaderField: "Accept")
 
-let task = session.dataTask(with: request as URLRequest, completionHandler: { data, response, error in
+
+let task = session.uploadTask(with: request as URLRequest,from: request.httpBody) {data, response,error in
     guard error == nil else{
         return
     }
     
-    guard let data = data else{
-        return
+//    print(response)
+    
+    if let data = data, let dataString = String(data:data,encoding: .utf8) {
+        print(dataString)
     }
     
-    do{
-        if let json = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as?[String: Any]{
-            print(json)
-        }
-    } catch let error{
-        print(error.localizedDescription)
-    }
-})
+
+}
 task.resume()
 
 
