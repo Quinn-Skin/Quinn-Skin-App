bce399847c59c364a587a00c06c5a138b2d7689d
Brayhan De Aza <brayhandeaza@gmail.com>
2020-12-03 11:28:14 -0500

    ui styled

--- Quinn-ios/Quinn-ios/controllers/BudgetViewController.swift
+++ Quinn-ios/Quinn-ios/controllers/BudgetViewController.swift
@@ -8,15 +8,20 @@
 import UIKit
 
 class BudgetViewController: UIViewController {
+    @IBOutlet var VCView: UIView!
     @IBOutlet weak var twentyFiveButton: UIButton!
     @IBOutlet weak var twentyFiveToFiftyButton: UIButton!
     @IBOutlet weak var overFiftyButton: UIButton!
     @IBOutlet weak var nextButton: UIButton!
     let cornerRadius = 20
-    var answer = ""
+
+//  colors
+    let tungstenColor = UIColor(red: 117.0/255.0, green: 110.0/255.0, blue: 53.0/255.0, alpha: 1.0)
+    let darkColor = UIColor(red: 32.0/255.0, green: 34.0/255.0, blue: 37.0/255.5, alpha: 1.0)
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         twentyFiveButton.layer.cornerRadius = CGFloat(cornerRadius)
         twentyFiveToFiftyButton.layer.cornerRadius = CGFloat(cornerRadius)
         overFiftyButton.layer.cornerRadius = CGFloat(cornerRadius)
@@ -24,10 +29,10 @@ class BudgetViewController: UIViewController {
     }
     
     @IBAction func onTwintyFiveOrLess(_ sender: Any) {
-        twentyFiveButton.layer.backgroundColor = UIColor.red.cgColor
+        twentyFiveButton.layer.backgroundColor =  UIColor(red: 177, green: 110, blue: 53, alpha: 1).cgColor
         twentyFiveToFiftyButton.layer.backgroundColor = UIColor.blue.cgColor
         overFiftyButton.layer.backgroundColor = UIColor.blue.cgColor
-        self.answer = "25 or Less"
+        
         AnswersManager.shared.answers["price"] = 25
         print(AnswersManager.shared.answers["price"]!!)
     }
@@ -36,7 +41,6 @@ class BudgetViewController: UIViewController {
         twentyFiveButton.layer.backgroundColor = UIColor.blue.cgColor
         twentyFiveToFiftyButton.layer.backgroundColor = UIColor.red.cgColor
         overFiftyButton.layer.backgroundColor = UIColor.blue.cgColor
-        self.answer = "$25-$50"
         AnswersManager.shared.answers["price"] = 50
         print(AnswersManager.shared.answers["price"]!!)
     }
@@ -45,7 +49,7 @@ class BudgetViewController: UIViewController {
         twentyFiveButton.layer.backgroundColor = UIColor.blue.cgColor
         twentyFiveToFiftyButton.layer.backgroundColor = UIColor.blue.cgColor
         overFiftyButton.layer.backgroundColor = UIColor.red.cgColor
-        self.answer = "Over $50"
+        
         AnswersManager.shared.answers["price"] = 60
         print(AnswersManager.shared.answers["price"]!!)
     }
