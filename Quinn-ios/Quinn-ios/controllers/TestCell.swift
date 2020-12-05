76c1b6dd7fd9877d8c9dfcfe36402de2f59fefbf
Brayhan De Aza <brayhandeaza@gmail.com>
2020-12-05 16:41:42 -0500

    can tracks answers

---
+++ Quinn-ios/Quinn-ios/controllers/TestCell.swift
@@ -0,0 +1,13 @@
+//
+//  TestCell.swift
+//  Quinn-ios
+//
+//  Created by Brayhan De Aza on 12/5/20.
+//
+
+import UIKit
+
+class TestCell: UICollectionViewCell {
+
+    @IBOutlet weak var answerButton: UILabel!
+}
