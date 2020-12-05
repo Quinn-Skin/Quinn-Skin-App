1954061d35abbe756216260f701000f710c8b529
Brayhan De Aza <brayhandeaza@gmail.com>
2020-12-05 11:47:40 -0500

    view is styled

--- Quinn-ios/Quinn-ios/controllers/RecommendedProductViewController.swift
+++ Quinn-ios/Quinn-ios/controllers/RecommendedProductViewController.swift
@@ -8,15 +8,19 @@ import AlamofireImage
 
 class RecommendedProductViewController: UIViewController {
     @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productBrandName: UILabel!
     @IBOutlet weak var productName: UILabel!
-    @IBOutlet weak var productDescription: UILabel!
+    @IBOutlet weak var productDescription: UITextView!
+    @IBOutlet weak var viewDetailsButton: UIButton!
+    //    @IBOutlet weak var circleView: UIView!
     var produts = [String : Any]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        viewDetailsButton.layer.cornerRadius = 10
         
-        let productId = AnswersManager.shared.productId.dropFirst(1).dropLast(2) as Substring
-    
+//        let productId = AnswersManager.shared.productId.dropFirst(1).dropLast(2) as Substring
+        let productId = "1"
         guard let url = URL(string: "https://quinn-server.herokuapp.com/products/\(productId)") else { return }
         
         let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
@@ -29,21 +33,24 @@ class RecommendedProductViewController: UIViewController {
             } else if let data = data {
                 let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
                 self.produts = dataDictionary["Produt"] as! [String : Any]
-                print(self.produts)
+//                print(self.produts)
 
                 // Product Image
                 let imageUrl = self.produts["image_url"] as? String
-                let formatedImageUrl = URL(string: imageUrl!)
+                let formatedImageUrl = URL(string: "https://www.sephora.com/productimages/sku/s2002087-main-zoom.jpg?imwidth=600")
                 self.productImage.af_setImage(withURL: formatedImageUrl!)
 
-                // Product name
+//                // Product name
                 let productNameText = self.produts["brand_name"] as? String
-                self.productName.text = productNameText?.capitalized
-
-                // Product description
+                self.productBrandName.text = productNameText?.capitalized
+              
+//                // Product description
                 self.productDescription.text = self.produts["description"] as? String
             }
          }
          task.resume()
+    }
+    @IBAction func sharedProduct(_ sender: Any) {
+        
     }
 }
